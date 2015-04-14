225883403dd773db248996b324c1a92411a1477c
roachdesign <mike@roachdesign.com>
2015-04-14 18:41:21 -0400

    Style Tweaks

--- footer.php
+++ footer.php
@@ -57,11 +57,10 @@
         <nav class="footer-nav"><?php wp_nav_menu(array('menu' => 'Primary' )); ?></nav>
       </div>
     </div>
-      &copy; <?php echo date("Y"); ?> <?php bloginfo('name'); ?> 
 </footer>
   <!-- Closing Container -->
   </div>
-
+  <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
   <script type="text/javascript">
 	$("#mobile-open").click(function(){
 	$("html").addClass("navigation-open");
@@ -71,7 +70,7 @@
 	});
   </script>
   <?php wp_footer(); ?>
- 
+  <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
   <!-- Bootstrap JS
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script> -->
 </body>
