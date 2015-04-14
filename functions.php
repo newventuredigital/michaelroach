493da246e65fa0945c2897bf1b9f0145b051651e
roachdesign <mike@roachdesign.com>
2015-04-14 18:10:35 -0400

    Enqueue jQuery

--- functions.php
+++ functions.php
@@ -1,4 +1,11 @@
 <?php
+  if (!is_admin()) add_action("wp_enqueue_scripts", "my_jquery_enqueue", 11);
+function my_jquery_enqueue() {
+   wp_deregister_script('jquery');
+   wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js", false, null);
+   wp_enqueue_script('jquery');
+}
+
 	// Declare sidebar widget zone
     if (function_exists('register_sidebar')) {
     	register_sidebar(array(
