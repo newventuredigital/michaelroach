c713592424a1d7f9e22c198b3c9f8052a1bc73af
roachdesign <mike@roachdesign.com>
2015-05-22 12:34:48 -0400

    Removed Masterslider

--- footer.php
+++ footer.php
@@ -70,7 +70,7 @@
 	});
   </script>
   <?php wp_footer(); ?>
-  <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+  <!--<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>-->
   <!-- Bootstrap JS
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script> -->
 </body>
---
+++ page-work.php
@@ -0,0 +1,62 @@
+<?php
+/*
+Template Name: Work
+*/
+?>
+<?php get_header(); ?>
+
+<?php $work = get_bloginfo('stylesheet_directory') . "/" . "work"; ?>
+<div class="content">
+	<section class="light-gray-bg">
+		<div class="container">
+			<div class="row" id="post-<?php the_ID(); ?>">
+				
+					<div class="container">
+					<div class="row gallery">
+						<div class="col-md-6">
+							<a href="<?php echo $work ?>/tlf-logo.gif" rel="prettyPhoto"><img src="<?php echo $work ?>/tlf-logo.gif" alt="" class="img-responsive"></a>
+						</div>
+						<div class="col-md-6">
+							<a href="<?php echo $work ?>/target-local-first-3.jpg" rel="prettyPhoto"><img src="<?php echo $work ?>/target-local-first-3.jpg" alt="" class="img-responsive"></a>
+						</div>
+						<div class="col-md-6">
+							<a href="<?php echo $work ?>/development-institute-1.jpg" rel="prettyPhoto"><img src="<?php echo $work ?>/development-institute-1.jpg" alt="" class="img-responsive"></a>
+						</div>
+						<div class="col-md-6">
+							<a href="<?php echo $work ?>/development-institute-2.jpg" rel="prettyPhoto"><img src="<?php echo $work ?>/development-institute-2.jpg" alt="" class="img-responsive"></a>
+						</div>
+
+						<div class="col-md-6">
+							<a href="<?php echo $work ?>/login-form.jpg" rel="prettyPhoto"><img src="<?php echo $work ?>/login-form.jpg" alt="" class="img-responsive"></a>
+						</div>
+						<div class="col-md-6">
+							<a href="<?php echo $work ?>/larson-brothers.jpg" rel="prettyPhoto"><img src="<?php echo $work ?>/larson-brothers.jpg" alt="" class="img-responsive"></a>
+						</div>
+						<div class="col-md-6">
+							<a href="<?php echo $work ?>/aarons-ne-logo.jpg" rel="prettyPhoto"><img src="<?php echo $work ?>/aarons-ne-logo.jpg" alt="" class="img-responsive"></a>
+						</div>
+						<div class="col-md-6">
+							<a href="<?php echo $work ?>/switch-logo.jpg" rel="prettyPhoto"><img src="<?php echo $work ?>/switch-logo.jpg" alt="" class="img-responsive"></a>
+						</div>
+						<div class="col-md-6">
+							<a href="<?php echo $work ?>/prosportsmd.jpg" rel="prettyPhoto"><img src="<?php echo $work ?>/prosportsmd.jpg" alt="" class="img-responsive"></a>
+						</div>
+						<div class="col-md-6">
+							<a href="<?php echo $work ?>/word-mavenry.jpg" rel="prettyPhoto"><img src="<?php echo $work ?>/word-mavenry.jpg" alt="" class="img-responsive"></a>
+						</div>
+						<div class="col-md-6">
+							<a href="<?php echo $work ?>/whiskey-willies-1.jpg" rel="prettyPhoto"><img src="<?php echo $work ?>/whiskey-willies-1.jpg" alt="" class="img-responsive"></a>
+						</div>
+						<div class="col-md-6">
+							<a href="<?php echo $work ?>/wayne-roach.jpg" rel="prettyPhoto"><img src="<?php echo $work ?>/wayne-roach.jpg" alt="" class="img-responsive"></a>
+						</div>
+					</div>
+					</div>
+				
+		 	</div>
+		 </div>
+	</section>
+
+
+
+<?php get_footer(); ?>
