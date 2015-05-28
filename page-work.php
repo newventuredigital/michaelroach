e00044d34f25a6bf466d41747348858e8bf4a7f3
roachdesign <mike@roachdesign.com>
2015-05-27 22:43:04 -0400

    Added JCH Logo

--- page-work.php
+++ page-work.php
@@ -14,41 +14,57 @@ Template Name: Work
 					<div class="container">
 					<div class="row gallery">
 						<div class="col-md-6">
+							<a href="<?php echo $work ?>/jch-mobile.gif" rel="prettyPhoto"><img src="<?php echo $work ?>/jch-mobile.gif" alt="" class="img-responsive"></a>
+							<div class="caption">Logo for Youtube Channel</div>
+						</div>
+						<div class="col-md-6">
 							<a href="<?php echo $work ?>/tlf-logo.gif" rel="prettyPhoto"><img src="<?php echo $work ?>/tlf-logo.gif" alt="" class="img-responsive"></a>
+							<div class="caption">Logo for location-based Marketing Firm</div>
 						</div>
 						<div class="col-md-6">
 							<a href="<?php echo $work ?>/target-local-first-3.jpg" rel="prettyPhoto"><img src="<?php echo $work ?>/target-local-first-3.jpg" alt="" class="img-responsive"></a>
+							<div class="caption">Homepage for location-based Marketing Firm</div>
 						</div>
 						<div class="col-md-6">
 							<a href="<?php echo $work ?>/development-institute-1.jpg" rel="prettyPhoto"><img src="<?php echo $work ?>/development-institute-1.jpg" alt="" class="img-responsive"></a>
+							<div class="caption">Homepage for web-based e-learning site</div>
 						</div>
 						<div class="col-md-6">
 							<a href="<?php echo $work ?>/development-institute-2.jpg" rel="prettyPhoto"><img src="<?php echo $work ?>/development-institute-2.jpg" alt="" class="img-responsive"></a>
+							<div class="caption">Session Page for web-based e-learning site</div>
 						</div>
 
 						<div class="col-md-6">
 							<a href="<?php echo $work ?>/login-form.jpg" rel="prettyPhoto"><img src="<?php echo $work ?>/login-form.jpg" alt="" class="img-responsive"></a>
+							<div class="caption">Form UI Concept</div>
 						</div>
 						<div class="col-md-6">
 							<a href="<?php echo $work ?>/larson-brothers.jpg" rel="prettyPhoto"><img src="<?php echo $work ?>/larson-brothers.jpg" alt="" class="img-responsive"></a>
+							<div class="caption">Homepage for Property Maintenance Company</div>
 						</div>
 						<div class="col-md-6">
-							<a href="<?php echo $work ?>/aarons-ne-logo.jpg" rel="prettyPhoto"><img src="<?php echo $work ?>/aarons-ne-logo.jpg" alt="" class="img-responsive"></a>
+							<a href="<?php echo $work ?>/aarons-sealcoating.gif" rel="prettyPhoto"><img src="<?php echo $work ?>/aarons-sealcoating.gif" alt="" class="img-responsive"></a>
+							<div class="caption">Logo for Asphalt Sealcoating company serving the Northeast US</div>
 						</div>
 						<div class="col-md-6">
-							<a href="<?php echo $work ?>/switch-logo.jpg" rel="prettyPhoto"><img src="<?php echo $work ?>/switch-logo.jpg" alt="" class="img-responsive"></a>
+							<a href="<?php echo $work ?>/switch-logo.gif" rel="prettyPhoto"><img src="<?php echo $work ?>/switch-logo.gif" alt="" class="img-responsive"></a>
+							<div class="caption">Student Work, Logo for Skateboard Shop</div>
 						</div>
 						<div class="col-md-6">
 							<a href="<?php echo $work ?>/prosportsmd.jpg" rel="prettyPhoto"><img src="<?php echo $work ?>/prosportsmd.jpg" alt="" class="img-responsive"></a>
+							<div class="caption">Logo for Doctor Specializing in Sports Medicine</div>
 						</div>
 						<div class="col-md-6">
 							<a href="<?php echo $work ?>/word-mavenry.jpg" rel="prettyPhoto"><img src="<?php echo $work ?>/word-mavenry.jpg" alt="" class="img-responsive"></a>
+							<div class="caption">Homepage for Proofreading Service</div>
 						</div>
 						<div class="col-md-6">
 							<a href="<?php echo $work ?>/whiskey-willies-1.jpg" rel="prettyPhoto"><img src="<?php echo $work ?>/whiskey-willies-1.jpg" alt="" class="img-responsive"></a>
+							<div class="caption">Homepage for Bar</div>
 						</div>
 						<div class="col-md-6">
 							<a href="<?php echo $work ?>/wayne-roach.jpg" rel="prettyPhoto"><img src="<?php echo $work ?>/wayne-roach.jpg" alt="" class="img-responsive"></a>
+							<div class="caption">Homepage for General Contractor</div>
 						</div>
 					</div>
 					</div>
