486e9558a5baea58d57c2dc9b72703443bd86f80
roachdesign <mike@roachdesign.com>
2015-05-28 11:59:36 -0400

    Wrong Width

--- single.php
+++ single.php
@@ -4,7 +4,7 @@
 	<section class="">
 		<div class="container">
 			<div class="row" id="post-<?php the_ID(); ?>">
-				<div class="col-md-8">
+				<div class="col-md-10">
 					<h1><?php the_title(); ?></h1>
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 						<?php the_content(); ?>
